557b49c376bfdd55abaf8f5057dd174e4b59b74a
yanggeng <yanggeng@mfexcel.com>
2021-07-03 17:39:52 +0800

    feat 新增账号学校关联表

---
+++ src/main/java/com/spring/security/dao/SysUserSchoolRelationDao.java
@@ -0,0 +1,17 @@
+package com.spring.security.dao;
+
+import com.spring.security.entity.SysUserSchoolRelation;
+
+public interface SysUserSchoolRelationDao {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysUserSchoolRelation record);
+
+    int insertSelective(SysUserSchoolRelation record);
+
+    SysUserSchoolRelation selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysUserSchoolRelation record);
+
+    int updateByPrimaryKey(SysUserSchoolRelation record);
+}
