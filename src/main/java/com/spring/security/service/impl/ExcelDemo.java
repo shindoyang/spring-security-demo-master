f4b652612fed16d302d5fb828b94963ac1e00e32
shindoyang <727792353@qq.com>
2021-07-05 07:19:11 +0800

    提交测试用例

--- src/main/java/com/spring/security/service/impl/ExcelDemo.java
+++ src/main/java/com/spring/security/service/impl/ExcelDemo.java
@@ -6,23 +6,34 @@ import com.alibaba.excel.ExcelWriter;
 import com.alibaba.excel.support.ExcelTypeEnum;
 import com.spring.security.entity.NmsSmsTmplExcelVo;
 import org.junit.Test;
+import org.springframework.util.StopWatch;
 
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ExcelDemo {
 
     @Test
     public void reloadExcel() throws Exception {
+        StopWatch stopWatch = new StopWatch();
         String fileName = "D:/cellphoneSampleSMS.xlsx";
+        stopWatch.start();
         List<NmsSmsTmplExcelVo> list = EasyExcel.read(fileName).head(NmsSmsTmplExcelVo.class).sheet(0).headRowNumber(2).doReadSync();
         System.out.println(list.size());
+        List<NmsSmsTmplExcelVo> newList = new ArrayList<>();
+        for (NmsSmsTmplExcelVo nmsSmsTmplExcelVo : list) {
+            nmsSmsTmplExcelVo.setText5("测试数据插入");
+            newList.add(nmsSmsTmplExcelVo);
+        }
 
 
-        OutputStream out = new FileOutputStream("D:/test2.xlsx");
-        ExcelWriter writer = EasyExcelFactory.getWriterWithTemp(inputStream, out, ExcelTypeEnum.XLSX, true);
+//        OutputStream out = new FileOutputStream("D:/test2.xlsx");
+        //ExcelWriter writer = EasyExcelFactory.getWriterWithTemp(inputStream, out, ExcelTypeEnum.XLSX, true);
 
-        EasyExcel.write("D:/test1.xlsx", NmsSmsTmplExcelVo.class).sheet("Student").doWrite(list);
+        EasyExcel.write("D:/test1.xlsx", NmsSmsTmplExcelVo.class).sheet("Student").doWrite(newList);
+        stopWatch.stop();
+        System.out.println("总耗时："+stopWatch.getTotalTimeSeconds());
     }
 }
